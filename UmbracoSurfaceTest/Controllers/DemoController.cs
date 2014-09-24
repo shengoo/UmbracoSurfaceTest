6220364e4c98b75d3636d0b2d4b8f4e47e0ae8b7
shengoo <sheng00>
2014-09-24 16:46:05 +0800

    first commit

---
+++ UmbracoSurfaceTest/Controllers/DemoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace UmbracoSurfaceTest.Controllers
+{
+    public class DemoController : SurfaceController
+    {
+        // url ~/umbraco/surface/Demo/Index/You
+        public ActionResult Index(string id)
+        {
+            return Content("hello " + id + "!");
+        }
+
+        // url ~/umbraco/surface/Demo/ViewTest
+        public ActionResult ViewTest()
+        {
+            return View();
+        }
+    }
+}
